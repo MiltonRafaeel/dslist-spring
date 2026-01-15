ae013b27983cc9e543091a4cd138269bde74085c
MiltonRafael <miltonrafael166@gmail.com>
2026-01-15 19:01:03 -0300

    Game repository, service, controller

---
+++ src/main/java/com/mrafaeloliveira/dslist/dto/GameMinDTO.java
@@ -0,0 +1,51 @@
+package com.mrafaeloliveira.dslist.dto;
+
+import com.mrafaeloliveira.dslist.entities.Game;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class GameMinDTO {
+	
+	private Long id;
+	private String title;	
+	private Integer year;	
+	private String imgUrl;
+	private String shortDescription;
+	
+	public GameMinDTO() {
+		super();
+	}
+
+	public GameMinDTO(Game entity) {
+		super();
+		id = entity.getId();
+		title = entity.getTitle();
+		year = entity.getYear();
+		imgUrl = entity.getImgUrl();
+		shortDescription = entity.getShortDescription();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public Integer getYear() {
+		return year;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	
+}
